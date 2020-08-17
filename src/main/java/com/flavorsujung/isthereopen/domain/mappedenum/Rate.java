79c867f71d55245937f65a96b271d6cc8f4f9def
YeoKyeong <yeokeong99@gmail.com>
2020-08-17 16:53:07 +0900

    Rate, Toilet이넘

--- src/main/java/com/flavorsujung/isthereopen/domain/mappedenum/Rate.java
+++ src/main/java/com/flavorsujung/isthereopen/domain/mappedenum/Rate.java
@@ -1,2 +1,23 @@
-package com.flavorsujung.isthereopen.domain.mappedenum;public enum Rate {
+package com.flavorsujung.isthereopen.domain.mappedenum;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum Rate {
+    WORST(0),
+    BAD(1),
+    SOSO(2),
+    GOOD(3),
+    BEST(4);
+
+    private final int rate;
+
+    public static Rate valueOf(int rate) {
+        for(Rate p : values())
+            if(p.getRate() == rate)
+                return p;
+        return null;
+    }
 }
--- src/main/java/com/flavorsujung/isthereopen/domain/mappedenum/ToiletConverter.java
+++ src/main/java/com/flavorsujung/isthereopen/domain/mappedenum/ToiletConverter.java
@@ -1,2 +1,15 @@
-package com.flavorsujung.isthereopen.domain.mappedenum;public class ToiletConverter {
+package com.flavorsujung.isthereopen.domain.mappedenum;
+
+import javax.persistence.AttributeConverter;
+
+public class ToiletConverter implements AttributeConverter<Toilet,Integer> {
+    @Override
+    public Integer convertToDatabaseColumn(Toilet toilet) {
+        return toilet.getToilet();
+    }
+
+    @Override
+    public Toilet convertToEntityAttribute(Integer integer) {
+        return Toilet.valueOf(integer);
+    }
 }
